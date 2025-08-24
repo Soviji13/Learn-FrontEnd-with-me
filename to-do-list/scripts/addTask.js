6f8a07025e66092be592fb90c33179913d34ad01
Sofía Villalba Jiménez <sofiasivillalbajimenez@gmail.com>
2025-08-24 19:21:25 +0200

    Creacion de nuevo proyecto

---
+++ to-do-list/scripts/addTask.js
@@ -0,0 +1,64 @@
+// Obtenemos el input, la lista de tareas y el botón
+const input = document.getElementById("input-tarea");
+const contenedorTareas = document.getElementById("tasks-container");
+const add = document.getElementById("buttonAdd");
+
+// Función para añadir tarea pulsando botón
+add.addEventListener("click", function()
+{
+    if(input.value !== '')
+    {
+        // Creamos los elementos dentro de task-container
+        let li = document.createElement("li"); 
+        li.setAttribute("haTerminado", "false");
+        let finish = document.createElement("div");
+        finish.className = "terminada"; 
+        let moreOptions = document.createElement("div");
+        moreOptions.className = "more-options";
+
+        // Le añadimos su valor a cada uno
+        li.innerHTML = input.value;
+        moreOptions.innerHTML = "..."
+
+        // Creamos el contenedor de todo esto y le añadimos los elementos
+        let taskContainer = document.createElement("div");
+        taskContainer.className = "task-container";
+        taskContainer.appendChild(li);
+        taskContainer.appendChild(finish);
+        taskContainer.appendChild(moreOptions);
+
+        contenedorTareas.appendChild(taskContainer);           
+        input.value = '';          
+    }
+}
+);
+
+// Función para añadir tarea pulsando enter mientras escribo en el input
+input.addEventListener("keydown", function(event)   // Al pulsar una tecla, se realiza la función cuyo parámetro es la tecla pulsada siempre que esté en input
+{
+    if (event.key === "Enter" && input.value !== '')
+    {
+        // Creamos los elementos dentro de task-container
+        let li = document.createElement("li");
+        li.setAttribute("haTerminado", "false");    
+        let finish = document.createElement("div");
+        finish.className = "terminada"; 
+        let moreOptions = document.createElement("div");
+        moreOptions.className = "more-options";
+
+        // Le añadimos su valor a cada uno
+        li.innerHTML = input.value;
+        moreOptions.innerHTML = "...";
+
+        // Creamos el contenedor de todo esto y le añadimos los elementos
+        let taskContainer = document.createElement("div");
+        taskContainer.className = "task-container";
+        taskContainer.appendChild(li);
+        taskContainer.appendChild(finish);
+        taskContainer.appendChild(moreOptions);
+
+        contenedorTareas.appendChild(taskContainer);           
+        input.value = '';   
+    }
+}
+);
---
+++ to-do-list/scripts/interactWhithTask.js
@@ -0,0 +1,63 @@
+// Variables
+
+// Cuando se pulse el botón de finish, la tarea se tachará y se pondrá más transparente (si la tarea no está terminada),
+// o volverá a como estaba (si la tarea está terminada)
+
+// Obtenemos el contenedor de tareas y observamos si se ha clickado - Si no hay aún ninguna tickada
+document.getElementById("tasks-container").addEventListener("click", function(event) 
+{
+    // Si se clickó, vemos si el evento que ha ocurrido es que hemos pulsado el botón con la clase .terminada
+    if (event.target.classList.contains("terminada")) 
+    {
+        // Obtenemos el contenedor asociado a esa casilla de terminada para acceder a su contenido (tarea)
+        const contenedorAsociado = event.target.parentElement;
+        const task = contenedorAsociado.querySelector("li");
+
+        // Tachamos la tarea si terminó - Además añadimos estilo visual al botón
+        if(task.getAttribute("haTerminado") === "false")
+        {
+            // Estilo del texto de tarea
+            task.style.textDecoration = "line-through";
+            task.style.opacity = 0.25;
+            task.setAttribute("haTerminado", "true");
+
+            // Creamos un nuevo div dentro del botón para añadirle decoración
+            let decoBoton = document.createElement("div");
+            decoBoton.className = "terminada-done";
+            event.target.appendChild(decoBoton);
+        }
+        // Volvemos la tarea a su estado normal si clickamos y estaba terminada
+        else
+        {
+            task.style.textDecoration = "none";
+            task.style.opacity = 1;
+            task.setAttribute("haTerminado", "false");
+
+            // Eliminamos el div quitándole la decoración
+            let decoBoton = event.target.querySelector(".terminada-done");
+            if (decoBoton) 
+            {
+                event.target.removeChild(decoBoton);
+            }
+        }
+
+    }
+
+    // Si se clickó, vemos si el evento que ha ocurrido es que hemos pulsado el botón con la clase .terminada-done
+    if (event.target.classList.contains("terminada-done")) 
+    {
+        // Obtenemos la tarea correspondiente
+        const casillaAsociada = event.target.parentElement;
+        const contenedorAsociado = casillaAsociada.parentElement;
+        const task = contenedorAsociado.querySelector("li");
+
+        // Le ponemos de nuevo que no ha terminado
+        task.style.textDecoration = "none";
+        task.style.opacity = 1;
+        task.setAttribute("haTerminado", "false");
+
+        // Eliminamos el botón de casilla asociada
+        let decoBoton = event.target;
+        casillaAsociada.removeChild(decoBoton);
+    }
+});
